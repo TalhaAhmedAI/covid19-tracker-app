6e4a8a9707a424dfb57424d7310355b77e7b9dd1
tester <aiforeveryoneai@gmail.com>
2020-07-02 12:21:24 +0500

    create func fetchData
    created a function name fetchData to fetch the data from api and called it within App.js inside useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
-import styles from "./App.module.css";
-
+import React, { useEffect } from "react";
 import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 function App() {
+  useEffect(() => {
+    async function getData() {
+      const data = await fetchData();
+      console.log(data);
+    }
+    getData();
+  });
   return (
     <div className={styles.container}>
       <Cards />
