7e47aa3a1a3f69644ad504817a96d5979a9b3312
tester <aiforeveryoneai@gmail.com>
2020-07-03 11:43:27 +0500

    update App.js
    added useState hook to App.js and passed the data to Cards component via props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
 
 function App() {
+  const [data, setData] = useState({});
   useEffect(() => {
     async function getData() {
-      const data = await fetchData();
-      console.log(data);
+      const response = await fetchData();
+      setData({ data: response });
     }
     getData();
   });
+
   return (
     <div className={styles.container}>
-      <Cards />
+      <Cards data={data} />
       <CountryPicker />
       <Chart />
     </div>
