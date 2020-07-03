02dbd6dbe40b5f79f38c4c88dbf2dd753a720007
tester <aiforeveryoneai@gmail.com>
2020-07-03 12:00:17 +0500

    update cards.js
    added a card component to cards.js and added cards.module.css

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,7 +1,30 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Grid,
+  StylesProvider,
+} from "@material-ui/core";
+import styles from "./cards.module.css";
 
 const Cards = (props) => {
-  return <h1> Cards </h1>;
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">Date</Typography>
+            <Typography>Number or Active cases of Covid-19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Cards;
