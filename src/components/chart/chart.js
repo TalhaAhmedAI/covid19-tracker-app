eca55549700df48bf773a938e47384c10c88d626
tester <aiforeveryoneai@gmail.com>
2020-07-04 04:19:59 +0500

    start work on chart
    wrote backend code for chart.js component and and start building the UI

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./chart.module.css";
 
 const Chart = () => {
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    const fetchURL = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    console.log(dailyData);
+
+    fetchURL();
+  }, []);
+
+  const LineChart = dailyData[0] ? (
+    <Line
+      data={{
+        labels: "",
+        datasets: [{}, {}],
+      }}
+    />
+  ) : null;
   return <h1> Chart</h1>;
 };
 
