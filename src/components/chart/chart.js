7aa073db9d1294aa43234a36ee49d21df5eac5c3
tester <aiforeveryoneai@gmail.com>
2020-07-04 23:58:46 +0500

    Update chart.js
    Removed the Bar import to get my code error free for this push

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { fetchDailyData } from "../../api";
-import { Line, Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import styles from "./chart.module.css";
 
 const Chart = () => {
@@ -14,27 +14,29 @@ const Chart = () => {
     fetchURL();
   }, [dailyData]);
 
-  const LineChart = (dailyData.length ? (
+  const LineChart = dailyData.length ? (
     <Line
       data={{
-        labels: dailyData.map(({date}) => date),
-        datasets: [{
-          data: dailyData.map(({confirmed}) => confirmed),
-          label: 'Infected',
-          borderColor: '#3333ff',
-          fill: true
-        }, {
-          data: dailyData.map(({deaths}) => deaths),
-          label: 'Deaths',
-          borderColor: 'red',
-          backgroundColor: 'rgba(255, 0, 0, 0.5)', 
-          fill: true}],
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
+            fill: true,
+          },
+        ],
       }}
     />
-  ) : null)
-  return <div className={styles.container}>
-    {LineChart  }
-  </div>
+  ) : null;
+  return <div className={styles.container}>{LineChart}</div>;
 };
 
 export default Chart;
