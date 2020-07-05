0609963b865563959cf954e71b54ad5d320c8414
tester <aiforeveryoneai@gmail.com>
2020-07-06 02:09:28 +0500

    Add barChart
    Added barChart in chart.js and made some necessary changes in App.js and countryPicker.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { fetchData } from "./api";
 
 function App() {
   const [data, setData] = useState({});
+  const [country, setCountry] = useState({});
 
   useEffect(() => {
     async function getData() {
@@ -16,14 +17,15 @@ function App() {
 
   const handleCountryChange = async (country) => {
     const fetchedData = await fetchData(country);
-    console.log(fetchedData);
+    setData(fetchedData);
+    setCountry(country);
   };
 
   return (
     <div className={styles.container}>
       <Cards data={data} />
       <CountryPicker handleCountryChange={handleCountryChange} />
-      <Chart />
+      <Chart data={data} country={country} />
     </div>
   );
 }
--- src/components/countryPicker/countryPicker.js
+++ src/components/countryPicker/countryPicker.js
@@ -12,7 +12,6 @@ const CountryPicker = ({ handleCountryChange }) => {
     };
     fetchAPI();
   }, [setFetchedCountries]);
-  console.log(fetchedCountries);
 
   return (
     <FormControl className={styles.formControl}>
